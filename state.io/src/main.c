eddf0130b885737bab2edce3b300316973917be2
Ali <alishahali1382@gmail.com>
2022-01-22 06:56:56 +0330

    almost completed map background graphics

--- state.io/src/main.c
+++ state.io/src/main.c
@@ -13,6 +13,7 @@ const int MinPlayers=2, MaxPlayers=6;
 const int MinStateDistance=70; // maybe change it?
 
 // here are some shit functions C doesnt have :)
+int swap(int *x, int *y){ *x^=*y, *y^=*x, *x^=*y;}
 int min(int x, int y){ return (x<y?x:y);}
 int max(int x, int y){ return (x>y?x:y);}
 double dmin(double x, double y){ return (x<y?x:y);}
@@ -48,23 +49,49 @@ void GenerateRandomMap(struct State *states){ // uses global variable nn
 		if (bad) i--;
 	}
 }
+int A[Width][Height]; // global array needed for making background
 SDL_Texture* MakeBackGround(SDL_Renderer *renderer, struct State *states){
 	SDL_Texture *texture=SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, Width, Height);
 	SDL_SetRenderTarget(renderer, texture);
 
-	SDL_SetRenderDrawColor(renderer, 120, 60, 80, 255);
-	SDL_RenderClear(renderer);
+	// SDL_SetRenderDrawColor(renderer, 120, 60, 80, 255);
+	// SDL_RenderClear(renderer);
 	
-	// int color[nn];
-	// for (int i=0; i<nn; i++) color[i]=0xff000000+rand()%01000000;
-	// for (int x=0; x<Width; x++) for (int y=0; y<Height; y++){
-	// 	int bst=-1, val=1e9;
-	// 	for (int i=0; i<nn; i++);
-	// }
+	for (int x=0; x<Width; x++) for (int y=0; y<Height; y++){
+		int bst=-1, val=1e9;
+		for (int i=0; i<nn; i++){
+			int d=distance2(x, y, states[i].x, states[i].y);
+			if (d<val){
+				val=d;
+				bst=i;
+			}
+		}
+		assert(bst!=-1);
+		A[x][y]=min(n, bst);
+	}
+	
+	int *color=(int *) malloc((n+1)*sizeof(int));
+	for (int i=0; i<n; i++) color[i]=0xff000000+rand()%01000000;
+	color[n]=0xffdfb320;
+	for (int x=0; x<Width; x++) for (int y=0; y<Height; y++){
+		if (x<=0 || y<=0 || Width-1<=x || Height-1<=y)
+			pixelColor(renderer, x, y, 0xff000000);
+		else{
+			int f=0;
+			for (int i=-1; i<=1; i++) for (int j=-1; j<=1; j++){
+				if (A[x+i][y+j]==A[x][y]) continue ;
+				f=1;
+				// maybe use different condition here?
+			}
+			if (f) pixelColor(renderer, x, y, 0xff000000);
+			else pixelColor(renderer, x, y, color[A[x][y]]);
+		}
+	}
+	free(color); // free-ed 60 bytes :))
 
 
 	for (int i=0; i<nn; i++){
-		filledCircleColor(renderer, states[i].x, states[i].y, 15, 0xff20b3df);
+		filledCircleColor(renderer, states[i].x, states[i].y, 15, 0xff00ff00);
 		// printf("circle %d is at position (%d, %d)\n", i, states[i].x, states[i].y);
 	}
 
@@ -84,7 +111,7 @@ int handleEvents(){
 int main(){
 	srand(time(0));
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("workshop", 20, 20, Width, Height, SDL_WINDOW_OPENGL);
+	SDL_Window* window = SDL_CreateWindow("map generator", 20, 20, Width, Height, SDL_WINDOW_OPENGL);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	
 
@@ -93,6 +120,8 @@ int main(){
 	m=2;
 	struct State *states=(struct State *)malloc(nn*sizeof(struct State));
 	GenerateRandomMap(states);
+
+
 
 	SDL_Texture *background=MakeBackGround(renderer, states);
 
@@ -111,11 +140,9 @@ int main(){
 		
 		SDL_RenderPresent(renderer);
 
+		// SDL_Delay(1000/FPS-(SDL_GetTicks()-start_ticks));
 		while (SDL_GetTicks()-start_ticks < 1000 / FPS);
 	}
-
-	SDL_Surface S;
-	
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
